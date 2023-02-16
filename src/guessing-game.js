182067e2d4b33a680211ddeefaf75b5ffd324781
yusivacova <sivacovayulia@gmail.com>
2023-02-16 23:29:21 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.right + this.left) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.right = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
 
